e49476f05220e264e75062867dae879c9e33e197
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-06-28 16:36:24 +0530

    header page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route, Router, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className='App' >
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route exact path='/' element={<Login/>}/>
         </Routes>
