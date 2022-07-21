b4dacfe5902be75cc8bb83d7bd6015163c24e032
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-07-21 21:35:38 +0530

    play video when hover on viewer

--- src/Components/Viewers/Viewers.js
+++ src/Components/Viewers/Viewers.js
@@ -2,31 +2,46 @@ import React from 'react'
 import styled from 'styled-components'
 
 function Viewers() {
-  return (
-    <Container>
-          <Wrap>
-              <img src='/images/viewers-starwars.png' alt=''/>
-          </Wrap>
+    return (
+        <Container>
+            <Wrap>
+                <img src='/images/viewers-starwars.png' alt='' />
+                <video muted={true} autoPlay={true} loop={true} playInline={true}>
+                    <source src='/videos/1608229455-star-wars.mp4'></source>
+                </video>
+            </Wrap>
 
-          <Wrap>
-              <img src='/images/viewers-pixar.png' alt='' />
-          </Wrap>
+            <Wrap>
+                <img src='/images/viewers-pixar.png' alt='' />
+                <video muted={true} autoPlay={true} loop={true} playInline={true}>
+                    <source src='/videos/1564676714-pixar.mp4'></source>
+                </video>
+            </Wrap>
 
-          <Wrap>
-              <img src='/images/viewers-national.png' alt='' />
-          </Wrap>
+            <Wrap>
+                <img src='/images/viewers-national.png' alt='' />
+                <video muted={true} autoPlay={true} loop={true} playInline={true}>
+                    <source src='/videos/1564676296-national-geographic.mp4'></source>
+                </video>
+            </Wrap>
 
-          <Wrap>
-              <img src='/images/viewers-marvel.png' alt='' />
-          </Wrap>
+            <Wrap>
+                <img src='/images/viewers-marvel.png' alt='' />
+                <video muted={true} autoPlay={true} loop={true} playInline={true}>
+                    <source src='/videos/1564676115-marvel.mp4'></source>
+                </video>
+            </Wrap>
 
-          <Wrap>
-              <img src='/images/viewers-disney.png' alt='' />
-          </Wrap>
-    </Container>
-  )
+            <Wrap>
+                <img src='/images/viewers-disney.png' alt='' />
+                <video muted={true} autoPlay={true} loop={true} playInline={true}>
+                    <source src='/videos/1564674844-disney.mp4'></source>
+                </video>
+            </Wrap>
+        </Container>
+    )
 }
-const Container=styled.div`
+const Container = styled.div`
 margin-top:30px;
 padding:30px 0px 26px;
 display:grid;
@@ -37,13 +52,14 @@ grid-template-columns:repeat(5,minmax(0,1fr));
     grid-template-columns:repeat(1,minmax(0,1fr));
 }
 `
-const Wrap=styled.div`
+const Wrap = styled.div`
 padding-top:56.25%;
 border-radius:10px;
 position:relative;
 overflow:hidden;
 transition:all 250ms cubic-bezier(0.25,0.46,0.45,0.94);
 border:3px solid rgba(249,249,249,0.1);
+cursor:pointer;
 img{
     inset:0;
     height:100%;
@@ -52,7 +68,25 @@ img{
     object-fit:cover;
     display:block;
     z-index:1;
+    transition:opacity 500ms ease-in-out 0s;
+    
 }
+video{
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    z-index: 0;
+    opacity: 0;
+    top: 0px;
+}
+&:hover{
+        transform:scale(1.05);
+        video{
+            opacity:1;
+        }
+}
+
+
 `
 
 export default Viewers
